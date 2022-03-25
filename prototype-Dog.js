436a4569ecee8060f7c9ae240e48135f7ccb5010
JulieBenchuk <juliebenchuk@gmail.com>
2022-03-25 18:10:10 +0300

    Objects (dogs) are created with constructor

---
+++ prototype-Dog.js
@@ -0,0 +1,19 @@
+function Dog(name, weight, vaccinated) {
+    this.name = name;
+    this.weight = weight;
+    this.vaccinated = vaccinated;
+}
+let bobik = new Dog("Bobik", 15,false);
+let mukhtar = new Dog("Mukhtar", 35, true);
+let tuzik = new Dog("Tuzik", 9, false);
+
+Dog.prototype.allowBoarding = function () {
+    if (this.vaccinated) {
+        console.log(this.name + " can be transported!");
+    } else {
+        console.log(this.name + " is prohibited from being transported!");
+    }
+}
+
+tuzik.allowBoarding();
+mukhtar.allowBoarding();
